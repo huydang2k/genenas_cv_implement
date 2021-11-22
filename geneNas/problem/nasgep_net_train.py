7c58cf366156c4708174fb7d1c85fe03db12cf7d
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-11-22 22:34:30 +0700

    add precalculate

---
+++ geneNas/problem/nasgep_net_train.py
@@ -0,0 +1,240 @@
+from argparse import ArgumentParser
+from typing import Optional
+import torch.nn.functional as F
+from transformers import AdamW
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from network.nasgep_cell_net import NasgepCellNet
+from util.logger import ChromosomeLogger
+from typing import List
+from torch.utils.data import Dataset
+from argparse import ArgumentParser
+
+class NasgepNetRWE_multiObj(pl.LightningModule):
+    
+    def __init__(
+        self,
+        num_labels: int,
+        dropout: float = 0.1,
+        hidden_shape: List = [3,32,32],
+        N: int = 1,
+        input_size: int = 32,
+        num_val_dataloader: int = 1,
+        **kwargs,
+    ):
+        # print('init NasgepNetRWE')
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_shape = hidden_shape
+        self.N = N
+        self.save_hyperparameters()
+        padding_for_conv_3x3 = (1,1)
+        post_nasgep_cell_output_channel = self.hidden_shape[0]
+        self.num_labels = num_labels
+        self.num_val_dataloader = num_val_dataloader
+        self.cell_dropout = nn.Dropout(p=dropout)
+        self.chromosome_logger: Optional[ChromosomeLogger] = None
+        self.metric = None
+      
+        self.temp_output = None
+        self.embed = nn.Sequential(
+            nn.Conv2d(in_channels= hidden_shape[0], out_channels=self.hidden_shape[0], kernel_size=3,padding = padding_for_conv_3x3) 
+        )
+        self.cls_head = nn.Sequential(
+            nn.BatchNorm2d(post_nasgep_cell_output_channel),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.AvgPool2d(kernel_size = int(self.hidden_shape[1]/4) ),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(hidden_shape[0], num_labels),
+        )
+    
+    def init_model(self, cells, adfs):
+        nasgepcell_net = NasgepCellNet(
+            cells,
+            adfs,
+            self.hidden_shape,
+            self.N
+        )
+        for param in nasgepcell_net.parameters():
+            param.requires_grad = False
+       
+        self.add_module("nasgepcell_rwe_net", nasgepcell_net)
+    
+    def total_params(self):
+        return sum(p.numel() for p in self.nasgepcell_rwe_net.parameters())
+    
+    def forward(self, feature_map, mode = 'validate'):
+        if mode == 'inference':            
+            x = self.embed(feature_map)
+            x = self.nasgepcell_rwe_net(x)
+            x = self.cls_head(x)
+            return x
+        
+        if mode == 'pre_calculate':   
+            
+            x = self.embed(feature_map.unsqueeze(0).cuda())
+            x = self.nasgepcell_rwe_net(x)
+            return x.squeeze()
+        
+        if mode == 'validate':
+            x = self.cls_head(feature_map)
+            return x
+            
+    
+    def configure_optimizers(self):
+        "Prepare optimizer and schedule (linear warmup and decay)"
+        embed = self.embed
+        model = self.nasgepcell_rwe_net
+        cls = self.cls_head
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                
+                "params": 
+                # do not train normal & reduction cell
+                # [
+                #     p
+                #     for n, p in model.named_parameters()
+                #     if not any(nd in n for nd in no_decay)
+                # ]
+                # +
+                [
+                    p
+                    for n, p in cls.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ]
+                # + [
+                #     p
+                #     for n, p in embed.named_parameters()
+                #     if not any(nd in n for nd in no_decay)
+                # ]
+                ,
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": 
+                #[
+                #     p
+                #     for n, p in model.named_parameters()
+                #     if any(nd in n for nd in no_decay)
+                # ]
+                # + 
+                [
+                    p
+                    for n, p in cls.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ]
+                # + [
+                #     p
+                #     for n, p in embed.named_parameters()
+                #     if any(nd in n for nd in no_decay)
+                # ]
+                ,
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = AdamW(
+            optimizer_grouped_parameters,
+            lr=self.hparams.learning_rate,
+            eps=self.hparams.epsilon,
+        )
+
+        # scheduler = get_linear_schedule_with_warmup(
+        #     optimizer,
+        #     num_warmup_steps=self.hparams.warmup_steps,
+        #     num_training_steps=self.total_steps,
+        # )
+        # scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        # return [optimizer], [scheduler]
+        return optimizer
+    
+    def reset_weights(self):
+        self.cls_head.reset_parameters()
+    
+    def validation_step(self, batch, batch_idx):
+       
+        logits = self(batch[0]['feature_map'])
+        labels = batch[1]['labels']
+        onehot_labels = batch[1]['one_hot']
+        val_loss = F.cross_entropy(logits, onehot_labels)
+        if self.hparams.num_labels >= 1:
+            preds = torch.argmax(logits, dim=1)
+        elif self.hparams.num_labels == 1:
+            preds = logits.squeeze()
+
+        # labels = batch["labels"]
+
+        return {"loss": val_loss, "preds": preds, "labels": labels}
+    
+    def training_step(self, batch, batch_idx):
+        
+        return self.validation_step(batch, batch_idx)
+    
+    def validation_epoch_end(self, outputs):
+        # No multiple eval_splits
+        # Looking at you MNLI
+        
+        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
+        
+        # self.print(np.unique(preds, return_counts=True))
+        # self.print(np.unique(labels, return_counts=True))
+        loss = torch.stack([x["loss"] for x in outputs]).mean()
+        self.log("val_loss", loss, prog_bar=True)
+        metrics = self.metric.compute(predictions=preds, references=labels)
+        self.log_dict(metrics, prog_bar=True)
+        log_data = {f"val_loss": loss, "metrics": metrics, "epoch": self.current_epoch}
+        self.chromosome_logger.log_epoch(log_data)
+        
+        
+
+    @staticmethod
+    def add_learning_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--learning_rate", default=2e-5, type=float)
+        parser.add_argument("--epsilon", default=1e-8, type=float)
+        parser.add_argument("--warmup_steps", default=0, type=int)
+        parser.add_argument("--weight_decay", default=0.0, type=float)
+        return parser
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--N", default=1, type=int)
+        parser.add_argument("--hidden_shape", default= [3,32,32], nargs='+', type=int)
+        # parser.add_argument("--input_size", default= 32, type=int)
+        parser.add_argument("--dropout", default=0.1, type=float)
+        parser.add_argument("--use_simple_cls", action="store_true")
+        return parser
+    
+    def total_params(self):
+        return sum(p.numel() for p in self.recurrent_model.parameters())
+    
+    def init_chromosome_logger(self, logger: ChromosomeLogger):
+        self.chromosome_logger = logger
+
+    def init_metric(self, metric):
+        self.metric = metric
+    
+    @staticmethod
+    def add_learning_specific_args(parent_parser):
+        
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--learning_rate", default=2e-5, type=float)
+        parser.add_argument("--epsilon", default=1e-8, type=float)
+        parser.add_argument("--warmup_steps", default=0, type=int)
+        parser.add_argument("--weight_decay", default=0.0, type=float)
+        return parser
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--N", default=1, type=int)
+        parser.add_argument("--hidden_shape", default= [3,32,32], nargs='+', type=int)
+        # parser.add_argument("--input_size", default= 32, type=int)
+        parser.add_argument("--dropout", default=0.1, type=float)
+        return parser
+        
